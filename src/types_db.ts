37030d0411a7fff7e0d41448a3a5d4c7f1841afa
Jannes Rudnick <mail@jannesrudnick.com>
2025-04-23 10:31:59 +0200

    feat(db): pull db test schema

--- src/types_db.ts
+++ src/types_db.ts
@@ -9,7 +9,24 @@ export type Json =
 export type Database = {
   public: {
     Tables: {
-      [_ in never]: never
+      test: {
+        Row: {
+          created_at: string
+          id: string
+          test: string | null
+        }
+        Insert: {
+          created_at?: string
+          id?: string
+          test?: string | null
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          test?: string | null
+        }
+        Relationships: []
+      }
     }
     Views: {
       [_ in never]: never
